571459f1fedbcae1bfe6dc882dc23531c46359b6
Shaman Patel <155854558+shamanpatel@users.noreply.github.com>
2024-12-22 18:46:14 -0800

    Time: 0 ms (100.00%), Space: 45.5 MB (32.08%) - LeetHub

--- 0268-missing-number/0268-missing-number.java
+++ 0268-missing-number/0268-missing-number.java
@@ -1,17 +1,10 @@
 class Solution {
     public int missingNumber(int[] nums) {
-        Arrays.sort(nums); // Binary Search Approach
-        int low = 0;
-        int high = nums.length - 1;
-        while(low <= high){
-            int middle = low + (high - low) / 2;
-            if(nums[middle] == middle){
-                low = middle + 1;
-            }
-            else{
-                high = middle - 1;
-            }
+        int result = nums.length;
+        for (int i = 0; i < nums.length; i++) {
+            result ^= (i ^ nums[i]);
         }
-        return low;
+
+        return result;
     }
 }

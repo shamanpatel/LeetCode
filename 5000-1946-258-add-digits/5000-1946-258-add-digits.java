8ffec972c5623f9fd64511395cbc33237717cbfd
Shaman Patel <155854558+shamanpatel@users.noreply.github.com>
2025-01-28 09:42:17 -0800

    Time: 1 ms, Memory: 40.5 MB - LeetHub

---
+++ 5000-1946-258-add-digits/5000-1946-258-add-digits.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int addDigits(int num) {
+        if(num / 10 > 0){
+            int sum = 0;
+            while(num > 0){
+                sum += num % 10;
+                num /= 10;
+            }
+            return addDigits(sum);
+        }
+        else{
+            return num;
+        }
+    }
+}

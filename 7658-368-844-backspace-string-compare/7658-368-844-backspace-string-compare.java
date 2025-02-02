cb2e54b3f7f78819b79e375e0966c270269e69e3
Shaman Patel <155854558+shamanpatel@users.noreply.github.com>
2025-02-02 14:28:10 -0800

    Time: 2 ms, Memory: 42.1 MB - LeetHub

---
+++ 7658-368-844-backspace-string-compare/7658-368-844-backspace-string-compare.java
@@ -0,0 +1,24 @@
+class Solution {
+    public boolean backspaceCompare(String s, String t) {
+        return buildString(s).equals(buildString(t));
+    }
+
+    private String buildString(String str) {
+        Stack<Character> stack = new Stack<>();
+        for (char ch : str.toCharArray()) {
+            if (ch == '#') {
+                if (!stack.isEmpty()) {
+                    stack.pop();
+                }
+            } else {
+                stack.push(ch);
+            }
+        }
+
+        StringBuilder sb = new StringBuilder();
+        while (!stack.isEmpty()) {
+            sb.append(stack.pop());
+        }
+        return sb.toString();
+    }
+}
